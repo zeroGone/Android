898e775ea321e7c66ef81a548e76f7a04323966f
YoungGon KIM <33260476+zeroGone@users.noreply.github.com>
2018-10-30 20:41:21 +0900

    1030 alert dialog

--- 모바일 프로그래밍1 강의자료/E02Views/app/src/main/java/net/shku/e02views/MainActivity.java
+++ 모바일 프로그래밍1 강의자료/E02Views/app/src/main/java/net/shku/e02views/MainActivity.java
@@ -43,7 +43,12 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(this, SpinnersActivity.class);
             startActivity(intent);
             return true;
+        } else if (id == R.id.action_alerts) {
+            Intent intent = new Intent(this, AlertsActivity.class);
+            startActivity(intent);
+            return true;
         }
+
 
         return super.onOptionsItemSelected(item);
     }
